a798fbf2db191744943daa3331e9f0700b4a4ddf
eric <xsephiroth@foxmail.com>
2020-02-26 16:08:25 +0800

    Setup pages router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
+const Home = lazy(() => import('./pages/Home'));
+const Billings = lazy(() => import('./pages/Billings'));
+
 function App() {
-  return <div>HiWallet2</div>;
+  return (
+    <Suspense fallback={<div className="fallback">Loading...</div>}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/billings" component={Billings} />
+        </Switch>
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
---
+++ src/pages/Billings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Billings = () => {
+  return (
+    <div className="Billings">
+      <h1>Billings</h1>
+    </div>
+  );
+};
+
+export default Billings;
