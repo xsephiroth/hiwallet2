0c8e9bae975eef6a7f938eb6e6b36f6fdd71cc06
eric <xsephiroth@foxmail.com>
2020-02-26 16:25:47 +0800

    Removed App styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { lazy, Suspense } from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.scss';
 
 const Home = lazy(() => import('./pages/Home'));
 const Billings = lazy(() => import('./pages/Billings'));
