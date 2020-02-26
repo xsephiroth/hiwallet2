868007ba62a08166ab14021e44ca9f3c2e6c4a2a
eric <xsephiroth@foxmail.com>
2020-02-26 16:42:34 +0800

    Home page basic header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Heading from '../components/Heading';
+import Card from '../components/Card';
+import './Home.scss';
+
+const Balance = () => {
+  return (
+    <Card className="Home__Balance">
+      <h1>Spent</h1>
+      <h1>Earn</h1>
+    </Card>
+  );
+};
 
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <Heading className="Home__Heading" round>
+        <h1>Some heading</h1>
+      </Heading>
+      <Balance />
     </div>
   );
 };
